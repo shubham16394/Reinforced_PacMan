3644c69ecb11e557c1fe9347334ec32199791317
shubham16394 <shubham16394@gmail.com>
2018-06-22 00:11:06 +0530

    Initial commit

---
+++ pacman_test.py
@@ -0,0 +1,111 @@
+import numpy as np
+import gym
+import os
+import tensorflow as tf
+import matplotlib
+import matplotlib.animation as animation
+import matplotlib.pyplot as plt
+plt.rcParams['axes.labelsize'] = 14
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['ytick.labelsize'] = 12
+
+env = gym.make("MsPacman-v0")
+obs = env.reset()
+
+
+mspacman_color = 210 + 164 + 74
+
+def preprocess_observation(obs):
+    img = obs[1:176:2, ::2] # crop and downsize
+    img = img.sum(axis=2) # to greyscale
+    img[img==mspacman_color] = 0 # Improve contrast
+    img = (img // 3 - 128).astype(np.int8) # normalize from -128 to 127
+    return img.reshape(88, 80, 1)
+
+
+tf.reset_default_graph()
+
+
+input_height = 88
+input_width = 80
+input_channels = 1
+conv_n_maps = [32, 64, 64]
+conv_kernel_sizes = [(8,8), (4,4), (3,3)]
+conv_strides = [4, 2, 1]
+conv_paddings = ["SAME"] * 3 
+conv_activation = [tf.nn.relu] * 3
+n_hidden_in = 64 * 11 * 10  # conv3 has 64 maps of 11x10 each
+n_hidden = 512
+hidden_activation = tf.nn.relu
+n_outputs = env.action_space.n  # 9 discrete actions are available
+initializer = tf.contrib.layers.variance_scaling_initializer()
+
+def q_network(X_state, name):
+    prev_layer = X_state / 128.0 # scale pixel intensities to the [-1.0, 1.0] range.
+    with tf.variable_scope(name) as scope:
+        for n_maps, kernel_size, strides, padding, activation in zip(
+                conv_n_maps, conv_kernel_sizes, conv_strides,
+                conv_paddings, conv_activation):
+            prev_layer = tf.layers.conv2d(
+                prev_layer, filters=n_maps, kernel_size=kernel_size,
+                strides=strides, padding=padding, activation=activation,
+                kernel_initializer=initializer)
+        last_conv_layer_flat = tf.reshape(prev_layer, shape=[-1, n_hidden_in])
+        hidden = tf.layers.dense(last_conv_layer_flat, n_hidden,
+                                 activation=hidden_activation,
+                                 kernel_initializer=initializer)
+        outputs = tf.layers.dense(hidden, n_outputs,
+                                  kernel_initializer=initializer)
+    trainable_vars = tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
+                                       scope=scope.name)
+    trainable_vars_by_name = {var.name[len(scope.name):]: var
+                              for var in trainable_vars}
+    return outputs, trainable_vars_by_name
+
+X_state = tf.placeholder(tf.float32, shape=[None, input_height, input_width,
+                                            input_channels])
+online_q_values, online_vars = q_network(X_state, name="q_networks/online")
+
+
+saver = tf.train.Saver()
+checkpoint_path = "./my_dqn.ckpt"
+
+frames = []
+n_max_steps = 10000
+
+with tf.Session() as sess:
+    saver.restore(sess, checkpoint_path)
+
+    obs = env.reset()
+    for step in range(n_max_steps):
+        state = preprocess_observation(obs)
+        
+
+        # Online DQN evaluates what to do
+        q_values = online_q_values.eval(feed_dict={X_state: [state]})
+        action = np.argmax(q_values)
+
+        # Online DQN plays
+        obs, reward, done, info = env.step(action)
+
+        img = env.render(mode="rgb_array")
+        frames.append(img)
+
+        if done:
+            break
+        
+def update_scene(num, frames, patch):
+    patch.set_data(frames[num])
+    return patch,
+
+def plot_animation(frames, repeat=False, interval=40):
+    plt.close()  # or else nbagg sometimes plots in the previous cell
+    fig = plt.figure()
+    patch = plt.imshow(frames[0])
+    plt.axis('off')
+    return animation.FuncAnimation(fig, update_scene, fargs=(frames, patch), frames=len(frames), repeat=repeat, interval=interval)    
+  
+video = plot_animation(frames)
+video.save('pacman_animationx.mp4', fps=30, extra_args=['-vcodec', 'libx264'])            
+            
+        
